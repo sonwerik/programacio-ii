58c36386caa2fa02299e6b4610af5f78c6884f56
sonwerik <sonwerik@gmail.com>
2024-11-27 12:40:10 +0100

    Modificacioó del nom de les variables amb final.

--- src/POO/Exercici5/Triangle.java
+++ src/POO/Exercici5/Triangle.java
@@ -1,28 +1,28 @@
 package POO.Exercici5;
 
 public class Triangle {
-    private final double stage1;
-    private final double stage2;
-    private final double stage3;
+    private final double STAGE_1;
+    private final double STAGE_2;
+    private final double STAGE_3;
 
     public Triangle(){
-        this.stage1 = 3.0;
-        this.stage2 = 4.0;
-        this.stage3 = 5.0;
+        this.STAGE_1 = 3.0;
+        this.STAGE_2 = 4.0;
+        this.STAGE_3 = 5.0;
     }
 
     public Triangle(double stage1, double stage2, double stage3) {
-        this.stage1 = stage1;
-        this.stage2 = stage2;
-        this.stage3 = stage3;
+        this.STAGE_1 = stage1;
+        this.STAGE_2 = stage2;
+        this.STAGE_3 = stage3;
     }
 
     public double perimetre() {
-        return stage1 + stage2 + stage3;
+        return STAGE_1 + STAGE_2 + STAGE_3;
     }
 
     public double area() {
-        double s = (stage1 + stage2 + stage3) / 2;
-        return Math.sqrt(s * (s - stage1) * (s - stage2) * (s - stage3));
+        double s = (STAGE_1 + STAGE_2 + STAGE_3) / 2;
+        return Math.sqrt(s * (s - STAGE_1) * (s - STAGE_2) * (s - STAGE_3));
     }
 }
