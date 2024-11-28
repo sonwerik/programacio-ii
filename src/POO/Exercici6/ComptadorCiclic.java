cf64cb8dd4a04ab2eaf819b57fb434a085d68a88
sonwerik <sonwerik@gmail.com>
2024-11-28 14:32:11 +0100

    Exercici 6 fet!

--- src/POO/Exercici6/ComptadorCiclic.java
+++ src/POO/Exercici6/ComptadorCiclic.java
@@ -13,26 +13,7 @@ public class ComptadorCiclic {
 
     public void incrementar() {
         if (this.comptador < 9) {comptador++;}
-        else {comptador = 0;}switch (opcio) {
-            case 1:
-                // Consultar el digit
-                System.out.println("Valor actual del digit: " + digit.consultar());
-                break;
-            case 2:
-                // Incrementar el digit
-                digit.incrementar();
-                System.out.println("Valor actual del digit: " + digit.consultar());
-                break;
-            case 3:
-                // Reset del digit
-                digit.reset();
-                System.out.println("Valor actual del digit: " + digit.consultar());
-                break;
-            default:
-                System.out.println("Opció no vàlida. Torna a intentar-ho.");
-                break;
-        }
-
+        else {comptador = 0;}
     }
 
     public void reset() {
