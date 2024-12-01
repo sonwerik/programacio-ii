9bb60d05fb8963a201eaae0564dbc7fb6f68b810
Èrik Lledó <sonwerik@gmail.com>
2024-12-01 21:02:33 +0100

    Exercici8

--- src/POO/Exercici8/Player.java
+++ src/POO/Exercici8/Player.java
@@ -1,4 +1,44 @@
 package POO.Exercici8;
 
 public class Player {
+    private Position position;
+    private String name;
+
+    public Player(Position position, String name) {
+        this.position = position;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void moveRight() {
+        position.incX();
+    }
+
+    public void moveLeft() {
+        position.decX();
+    }
+
+    public void jump() {
+        position.incY();
+    }
+
+    public void fall() {
+        position.decY();
+    }
+
+    @Override
+    public String toString() {
+        return "Player: " + name + ", Position: " + position;
+    }
 }
--- src/POO/Exercici8/Position.java
+++ src/POO/Exercici8/Position.java
@@ -1,4 +1,58 @@
 package POO.Exercici8;
 
 public class Position {
+    private int x;
+    private int y;
+
+    public Position() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void incX() {
+        this.x++;
+    }
+
+    public void incY() {
+        this.y++;
+    }
+
+    public void decX() {
+        this.x--;
+    }
+
+    public void decY() {
+        this.y--;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
