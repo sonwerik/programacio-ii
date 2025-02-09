d10c51b73f67622ca83dbb4ff09e98bf16e77a3b
Èrik Lledó <sonwerik@gmail.com>
2025-02-09 18:15:34 +0100

    updating folder name and adding bubble sort

---
+++ 3. Estructures de Dades/1. Hackerrank/Arrays/pt9BubbleSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class pt9BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String[] teams = new String[n];
+        int[] scores = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+            teams[i] = sc.next();
+            scores[i] = sc.nextInt();
+        }
+
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (scores[j] < scores[j + 1]) {
+
+                    int tempScore = scores[j];
+                    scores[j] = scores[j + 1];
+                    scores[j + 1] = tempScore;
+
+
+                    String tempTeam = teams[j];
+                    teams[j] = teams[j + 1];
+                    teams[j + 1] = tempTeam;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(teams[i] + " " + scores[i]);
+        }
+    }
+}
