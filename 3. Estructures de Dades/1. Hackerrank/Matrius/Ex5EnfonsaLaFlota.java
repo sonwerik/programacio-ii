9209186bb2b04b723f23f6d73df48d35d291e595
Èrik Lledó <sonwerik@gmail.com>
2025-02-09 18:20:18 +0100

    idk

--- 3. Estructures de Dades/1. Hackerrank/Matrius/Ex5EnfonsaLaFlota.java
+++ 3. Estructures de Dades/1. Hackerrank/Matrius/Ex5EnfonsaLaFlota.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class pt5EnfonsaLaFlota {
+public class Ex5EnfonsaLaFlota {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
